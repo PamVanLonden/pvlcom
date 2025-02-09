c82366a98a7481004b2960329b360db626909b5c
Pam Van Londen <pam.vanlonden@oregonstate.edu>
2025-02-09 12:38:31 -0800

    replaced missing files

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    fs: {
-      strict: false,
-    },
-  },
-});
+  // server.hmr.overlay: false
+})
