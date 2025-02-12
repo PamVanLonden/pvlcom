bfbe0daafb551618f1fe114930a0caf684125d93
Teresita Guzman Nader <TeresitaCGNader@users.noreply.github.com>
2025-02-11 18:32:23 -0800

    Moved everything related to cart to a folder named cart under /modules

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import CartContext from "./context/CartProvider.jsx";
+import CartContext from "./modules/cart/CartProvider.jsx"; 
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <CartContext>
       <App />
     </CartContext>
-  </StrictMode>,
+  </StrictMode>
 );
