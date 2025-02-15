0f0bce5b4ab346e287c829df873a4781069daed4
Pam Van Londen <pam.vanlonden@oregonstate.edu>
2025-02-14 16:58:05 -0800

    moved slogan to right location

---
+++ src/modules/Slogan.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { FaPaintBrush } from "react-icons/fa";
+import { TbMoodHappyFilled,  } from "react-icons/tb";
+import { IoCodeSlashSharp } from "react-icons/io5";
+
+function Slogan() {
+    return (
+    <>
+            Art <i><FaPaintBrush /></i>
+            &nbsp;+&nbsp;
+            Code <i><IoCodeSlashSharp /></i>
+            &nbsp;=&nbsp;
+            Joy <i><TbMoodHappyFilled /></i>
+        </>
+    );
+}
+export default Slogan;
