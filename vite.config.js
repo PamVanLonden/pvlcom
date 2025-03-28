1b16004972933ac25ddcf2050ba158c3b5ec2c68
Pam Van Londen <pam.vanlonden@oregonstate.edu>
2025-03-28 11:17:52 -0700

    seo updates

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import Sitemap from 'vite-plugin-sitemap';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/",
-  plugins: [react()],
+  plugins: [
+    react(),
+    Sitemap({ exclude: ['/cart', '/cart/venmo', '/cart/checkout-form', '/legal', '/social'] }),
+  ],
 });
