6d7ac3543451224ff164f5a5c546c70a71a0cd29
Pam Van Londen <pam.vanlonden@oregonstate.edu>
2025-03-28 17:47:42 -0700

    Add files via upload

--- src/modules/ContactPage.jsx
+++ src/modules/ContactPage.jsx
@@ -4,20 +4,22 @@ function ContactPage() {
     return (
         <div >
             <h2>Contact Me</h2>
+            <article>
             <h3>Text me for information.</h3>
             <p>Pam Van Londen</p>
             <p>Corvallis, OR 97330, USA</p>
             <p>
                 Tel: <a href="sms:+15417601449">541-760-1449</a> (Text only, please)
             </p>
-            <div className="contact-info">
+            <figure >
                 <img 
                     className="canvas-logo"
                     src="/assets/canvas-logo.png" 
                     alt="Login to Canvas" 
                 />
-                <p>Current OSU students: please contact me via Canvas Inbox.</p>
-            </div>
+                <figcaption>Current OSU students: please contact me via Canvas Inbox.</figcaption>
+            </figure>
+            </article>
         </div>
     );
 }
