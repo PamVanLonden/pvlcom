4f8d21cb4d46646db31ffe9f03d52f33d3bb2ce6
Pam Van Londen <pam.vanlonden@oregonstate.edu>
2025-04-22 16:11:30 -0700

    Share icons with links added

---
+++ src/hooks/SocialShare.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import {FaFacebook, FaLinkedin, FaCopy, FaInstagramSquare } from "react-icons/fa"; 
+import { RiBlueskyFill } from "react-icons/ri";
+import { BsThreadsFill } from "react-icons/bs";
+import { MdEmail } from "react-icons/md";
+
+const SocialShare = ({ url, title = '', text = '' }) => {
+  const share = (platform) => {
+    const encodedURL = encodeURIComponent(url);
+    const encodedTitle = encodeURIComponent(title);
+    const encodedText = encodeURIComponent(text);
+    let shareURL = '';
+
+    switch (platform) {
+      case 'bluesky':
+        shareURL = `https://bsky.app/intent/compose?text=${encodedText}%20${encodedURL}`;
+        break;
+      case 'facebook':
+        shareURL = `https://www.facebook.com/sharer/sharer.php?u=${encodedURL}`;
+        break;
+      case 'threads':
+        shareURL = `https://www.threads.net/intent/post?text=${encodedText}%20${encodedURL}`;
+        break;
+      case 'linkedin':
+        shareURL = `https://www.linkedin.com/shareArticle?mini=true&url=${encodedURL}&title=${encodedTitle}`;
+        break;
+      case 'email':
+        shareURL = `mailto:?subject=${encodedTitle}&body=${encodedText}%0A${encodedURL}`;
+        break;
+      case 'copy':
+        navigator.clipboard.writeText(url).then(() => {
+          alert('Link copied to clipboard!');
+        });
+        return;
+      default:
+        break;
+    }
+
+    if (shareURL) {
+      window.open(shareURL, '_blank', 'noopener,noreferrer');
+    }
+  };
+
+  return (
+    <div>
+        <a onClick={() => share('bluesky')} ><RiBlueskyFill /></a> 
+        <a href="https://instagram.com/pamvanlonden" target="_blank" rel="noreferrer"><FaInstagramSquare /></a>
+        <a onClick={() => share('facebook')} ><FaFacebook /></a>
+        <a onClick={() => share('threads')} ><BsThreadsFill /></a>
+        <a onClick={() => share('linkedin')} ><FaLinkedin /></a>
+        <a onClick={() => share('email')} ><MdEmail /></a>
+        <a onClick={() => share('copy')} ><FaCopy /></a>
+    </div>
+  );
+};
+
+export default SocialShare;
